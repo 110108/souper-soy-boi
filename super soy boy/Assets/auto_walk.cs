b51dc6a2a3adeee94165685202425d6ed3d0356f
110108 <31996737+110108@users.noreply.github.com>
2018-04-13 13:32:07 -0400

    ini

---
+++ super soy boy/Assets/auto_walk.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auto_walk : MonoBehaviour {
+	public Rigidbody2D ssb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ssb.AddForce(new Vector2(1.00f,0.10f));
+	}
+}
