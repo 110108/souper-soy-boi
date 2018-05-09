88b77974f8b2286f385e4d526080aaec051247e4
110108 <31996737+110108@users.noreply.github.com>
2018-05-09 13:38:36 -0400

    souper

--- super soy boy/Assets/Scripts/Goal.cs
+++ super soy boy/Assets/Scripts/Goal.cs
@@ -20,6 +20,7 @@ public class Goal : MonoBehaviour {
             //var timer = FindObjectOfType<Timer>();
             //Call the save time method in the game manager script and pass through the time from the timer script
             //GameManager.instance.RestartLevel(0.5f);
+			System.Threading.Thread.Sleep(1000);
 			SceneManager.LoadScene("Menu");
         }
     }
