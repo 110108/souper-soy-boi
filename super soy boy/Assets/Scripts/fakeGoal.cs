b5f606295e5abdcbcd3686a37725bcb091f32b62
110108 <31996737+110108@users.noreply.github.com>
2018-05-10 13:37:06 -0400

    iwatmhkhfu

--- super soy boy/Assets/Scripts/fakeGoal.cs
+++ super soy boy/Assets/Scripts/fakeGoal.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class fakeGoal : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+//			GameObject.SetActive=(false);
+		}
 	}
 }
