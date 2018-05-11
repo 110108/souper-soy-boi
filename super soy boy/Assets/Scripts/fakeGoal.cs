1360a0cf17c75c14826645efa4ce73cb1dcff5ad
110108 <31996737+110108@users.noreply.github.com>
2018-05-11 12:47:52 -0400

    earrape mod

--- super soy boy/Assets/Scripts/fakeGoal.cs
+++ super soy boy/Assets/Scripts/fakeGoal.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class fakeGoal : MonoBehaviour {
+	public GameObject This;
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 		if (coll.gameObject.tag == "Player") {
-//			GameObject.SetActive=(false);
+			gameObject.SetActive (false);
+			Debug.Log ("trigger");
 		}
+		if(gameObject.tag=="fakeout"){
+			Debug.Log ("trigger0");
+		}
+		Debug.Log ("trigger1");
+
 	}
 }
